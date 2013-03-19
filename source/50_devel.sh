5bf3eb9636f3e86ace9e8f00b597c9610bd35d64
John Cowhig Jr <jcowhigjr@Johns-MacBook-Pro.local>
2013-03-19 11:07:52 -0400

    trying to get rid of the rbenv part of dotfiles

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -1,65 +1,66 @@
-export PATH
+# export PATH
 
-# nave init.
-if [[ "$(type -P nave)" ]]; then
-  nave_default="$(nave ls | awk '/^default/ {print $2}')"
-  if [[ "$nave_default" && "$(node --version 2>/dev/null)" != "v$nave_default" ]]; then
-    node_path=~/.nave/installed/$nave_default/bin
-    if [[ -d "$node_path" ]]; then
-      PATH=$node_path:$(path_remove ~/.nave/installed/*/bin)
-    fi
-  fi
-fi
+# # nave init.
+# if [[ "$(type -P nave)" ]]; then
+#   nave_default="$(nave ls | awk '/^default/ {print $2}')"
+#   if [[ "$nave_default" && "$(node --version 2>/dev/null)" != "v$nave_default" ]]; then
+#     node_path=~/.nave/installed/$nave_default/bin
+#     if [[ -d "$node_path" ]]; then
+#       PATH=$node_path:$(path_remove ~/.nave/installed/*/bin)
+#     fi
+#   fi
+# fi
 
-npm_globals=(grunt jshint uglify-js codestream)
+# npm_globals=(grunt jshint uglify-js codestream)
 
-# Fetch and build the latest stable Node.js, assigning it the alias "default"
-alias nave_stable='nave use default stable nave_stable_2 $(node --version 2>/dev/null); src'
-function nave_stable_2() {
-  npm update -g npm
-  if [[ "$1" != "$(node --version 2>/dev/null)" ]]; then
-    echo "Node.js version updated to $1, installing Npm global modules."
-    npm install -g ${npm_globals[*]}
-  else
-    echo "Node.js version $1 unchanged."
-  fi
-}
+# # Fetch and build the latest stable Node.js, assigning it the alias "default"
+# alias nave_stable='nave use default stable nave_stable_2 $(node --version 2>/dev/null); src'
+# function nave_stable_2() {
+#   npm update -g npm
+#   if [[ "$1" != "$(node --version 2>/dev/null)" ]]; then
+#     echo "Node.js version updated to $1, installing Npm global modules."
+#     npm install -g ${npm_globals[*]}
+#   else
+#     echo "Node.js version $1 unchanged."
+#   fi
+# }
 
-# Publish module to Npm registry, but don't update "latest" unless the version
-# is an actual release version!
-function npm_publish() {
-  local version="$(node -pe 'require("./package.json").version' 2>/dev/null)"
-  if [[ "${version#v}" =~ [a-z] ]]; then
-    echo "Publishing dev version $version with --tag=devel --force"
-    npm publish --tag=devel --force "$@"
-  else
-    echo "Publishing new latest version $version"
-    npm publish "$@"
-  fi
-}
+# # Publish module to Npm registry, but don't update "latest" unless the version
+# # is an actual release version!
+# function npm_publish() {
+#   local version="$(node -pe 'require("./package.json").version' 2>/dev/null)"
+#   if [[ "${version#v}" =~ [a-z] ]]; then
+#     echo "Publishing dev version $version with --tag=devel --force"
+#     npm publish --tag=devel --force "$@"
+#   else
+#     echo "Publishing new latest version $version"
+#     npm publish "$@"
+#   fi
+# }
 
-# I needed to find a way to look at a project's package.json and figure out
-# what dependencies could be updated. This seemed easiest.
-function npm_update() {
-  local deps='JSON.parse(require("fs").readFileSync("package.json")).dependencies'
-  # Install the latest version of all dependencies listed in package.json.
-  echo 'Installing @latest version of all dependencies...'
-  npm install $(node -pe "Object.keys($deps).map(function(m){return m+'@latest'}).join(' ')");
-  # List all dependencies that are now invalid, along with their (new) version.
-  if npm ls | grep invalid >/dev/null; then
-    echo -e '\nINVALID DEPENDENCIES\n'
-    echo 'Module name:                   @latest:             package.json:'
-    npm ls | perl -ne "m/.{10}(.+)@(.+?) invalid\$/ && printf \"%-30s %-20s %s\", \$1, \$2, \`node -pe '$deps[\"\$1\"]'\`"
-    return 99
-  else
-    echo -e '\nNo invalid dependencies.'
-  fi
-}
+# # I needed to find a way to look at a project's package.json and figure out
+# # what dependencies could be updated. This seemed easiest.
+# function npm_update() {
+#   local deps='JSON.parse(require("fs").readFileSync("package.json")).dependencies'
+#   # Install the latest version of all dependencies listed in package.json.
+#   echo 'Installing @latest version of all dependencies...'
+#   npm install $(node -pe "Object.keys($deps).map(function(m){return m+'@latest'}).join(' ')");
+#   # List all dependencies that are now invalid, along with their (new) version.
+#   if npm ls | grep invalid >/dev/null; then
+#     echo -e '\nINVALID DEPENDENCIES\n'
+#     echo 'Module name:                   @latest:             package.json:'
+#     npm ls | perl -ne "m/.{10}(.+)@(.+?) invalid\$/ && printf \"%-30s %-20s %s\", \$1, \$2, \`node -pe '$deps[\"\$1\"]'\`"
+#     return 99
+#   else
+#     echo -e '\nNo invalid dependencies.'
+#   fi
+# }
 
+#commented out because boxen takes care of this 
 # rbenv init.
-PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
-PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
+#PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
+#PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
 
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
+#if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
+#  eval "$(rbenv init -)"
+#fi
