a3eb9a01c28b9dc7d3d0936a1275838ccfaec700
John <jcowhigjr+dev@gmail.com>
2013-07-17 16:36:03 -0400

    typo in keybindings

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias -g G='| grep'
-alias emacs=emacs --daemon
-alias e=emacsclient -t
-alias ec=emacsclient -c
-alias vim=emacsclient -t
-alias vi=emacsclient -t
+alias emacsd='emacs --daemon'
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
+alias vim='emacsclient -t'
+alias vi='emacsclient -t'
